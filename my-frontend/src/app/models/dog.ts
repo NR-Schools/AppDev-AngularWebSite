fc714f95de7b63af15da757fcb3e405ac12ce399
Tuna <98098486+Null-Root@users.noreply.github.com>
2023-10-20 18:46:46 +0800

    Add sample data for dogs

--- my-frontend/src/app/models/dog.ts
+++ my-frontend/src/app/models/dog.ts
@@ -1,27 +1,37 @@
 export class Dog {
-    id?: number;
     name: string;
     breed: string;
-    dob: Date;
+    age: number;
     sex: string;
-    height: number;
-    weight: number;
     colorCoat: string;
-    description?: string;
-    adoptDate?: Date;
+    description: string;
+    arrivedDate: Date;
+    arrivedFrom: string;
+    size: string;
+    location: string;
 
     constructor(
-        _name: string, _breed: string, _dob: Date, _sex: string,
-        _height: number, _weight: number, _colorCoat: string,
-        _description: string, _adoptDate: Date) {
+        _name: string,
+        _breed: string,
+        _age: number,
+        _sex: string,
+        _colorCoat: string,
+        _description: string,
+        _arrivedDate: Date,
+        _arrivedFrom: string,
+        _size: string,
+        _location: string
+    ) {
         this.name = _name;
         this.breed = _breed;
-        this.dob = _dob;
+        this.age = _age;
         this.sex = _sex;
-        this.height = _height;
-        this.weight = _weight;
         this.colorCoat = _colorCoat;
         this.description = _description;
-        this.adoptDate = _adoptDate;
+        this.arrivedDate = _arrivedDate;
+        this.arrivedFrom = _arrivedFrom;
+        this.size = _size;
+        this.location = _location;
     }
 }
+
--- my-frontend/src/app/services/dog-record.service.ts
+++ my-frontend/src/app/services/dog-record.service.ts
@@ -5,34 +5,44 @@ import { HttpClient } from '@angular/common/http';
 import { BaseService } from './base_service';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class DogRecordService extends BaseService {
-  constructor(private http: HttpClient) {
-    super();
-  }
 
-  addDogRecord(dog: Dog) {
-    // Send dog to server
-  }
+	dogList: Array<Dog> = [
+		new Dog('Buddy', 'Golden Retriever', 3, 'Male', 'Gold', 'Buddy is a friendly and playful dog. He loves to fetch and go on long walks.', new Date('2023-05-15'), 'Rescue Shelter', 'Medium', 'New York, NY'),
+		new Dog('Luna', 'Labrador Retriever', 2, 'Female', 'Black, short coat with a white blaze on her forehead', 'Luna is a gentle and well-behaved dog. She enjoys cuddling and playing with toys.', new Date('2023-04-20'), 'Foster Home', 'Large', 'Los Angeles, CA'),
+		new Dog('Rocky', 'German Shepherd', 4, 'Male', 'Sable, medium-length coat with a black mask', 'Rocky is a loyal and protective dog. He is great for families and enjoys outdoor activities.', new Date('2023-03-10'), 'Animal Rescue', 'Large', 'Chicago, IL'),
+		new Dog('Daisy', 'Dachshund', 5, 'Female', 'Red, short coat with a dark stripe down her back', 'Daisy is a sweet and energetic dog. She loves to play fetch and go on adventures.', new Date('2023-06-05'), 'Animal Shelter', 'Small', 'Houston, TX'),
+	];
 
-  viewAllDogRecords() {
-    // Get all dogs
-    // If user, return only dogs that are not yet adopted
-    // If admin, return all dogs
-    return this.http.get<Dog[]>(this.dogssUrl + '/dogs/');
-  }
+	constructor(private http: HttpClient) {
+		super();
+	}
 
-  viewDogRecord(dog_id: number) {
-    // Get specific dog recor
-  }
+	addDogRecord(dog: Dog): void {
+		// Send dog to server
+	}
 
-  updateDogRecord(dog_id: number, updated_dog_info: Dog) {
-    // Create new dog object, merging id and updated info
-    // Send dog to server
-  }
+	viewAllDogRecords(): Array<Dog> {
+		// Get all dogs
+		// If user, return only dogs that are not yet adopted
+		// If admin, return all dogs
+		//return this.http.get<Dog[]>(this.dogssUrl + '/dogs/');
+		return this.dogList;
+	}
 
-  deleteDogRecord(dog_id: number) {
-    // Delete dog from server
-  }
+	viewDogRecord(dog_id: number): Dog {
+		// Get specific dog record
+		return this.dogList[0];
+	}
+
+	updateDogRecord(dog_id: number, updated_dog_info: Dog): void {
+		// Create new dog object, merging id and updated info
+		// Send dog to server
+	}
+
+	deleteDogRecord(dog_id: number): void {
+		// Delete dog from server
+	}
 }
