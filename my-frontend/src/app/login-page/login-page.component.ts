0d94056c81ac850e030d17562e4bc3c2505faf94
Tuna <98098486+Null-Root@users.noreply.github.com>
2023-10-22 15:12:38 +0800

    Added Log In Implementation (FrontEnd)

--- my-frontend/src/app/login-page/login-page.component.ts
+++ my-frontend/src/app/login-page/login-page.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../services/account.service';
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+	selector: 'app-login-page',
+	templateUrl: './login-page.component.html',
+	styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
 
+	constructor(private formBuilder: FormBuilder,
+				private accountService: AccountService) { }
+
+	loginFormGroup = this.formBuilder.group({
+		Email: ['', Validators.required],
+		Password: ['', Validators.required],
+	});
+
+	loginAccount() {
+		// Get Values From Controls
+		const Email = this.loginFormGroup.get("Email")?.value as string;
+		const Password = this.loginFormGroup.get("Password")?.value as string;
+
+		// Login
+		this.accountService.login(Email, Password).subscribe({
+			next: (value: boolean) => {
+				if (value) window.alert("Account Logged In Successfully!");
+				else window.alert("Account Log In Failed!");
+			},
+			error: (err: any) => {
+				window.alert("Error encountered!");
+				console.log(err);
+			}
+		});
+	}
 }
