66ff4e27bcf447e3d65c2e092a37b42c34fa8e8c
Tuna <98098486+Null-Root@users.noreply.github.com>
2023-10-23 10:09:49 +0800

    Add Dog Details Connections

--- my-frontend/src/app/dog-details/dog-details.component.ts
+++ my-frontend/src/app/dog-details/dog-details.component.ts
@@ -4,8 +4,35 @@ import { Dog } from '../models/dog';
 import { DogRecordService } from '../services/dog-record.service';
 
 @Component({
-  selector: 'app-dog-details',
-  templateUrl: './dog-details.component.html',
-  styleUrls: ['./dog-details.component.css'],
+	selector: 'app-dog-details',
+	templateUrl: './dog-details.component.html',
+	styleUrls: ['./dog-details.component.css'],
 })
-export class DogDetailsComponent {}
+export class DogDetailsComponent implements OnInit {
+
+	dog: Dog;
+
+	constructor(private route: ActivatedRoute,
+				private dogRecordService: DogRecordService) {
+					this.dog = Dog.NoDog();
+				}
+
+	ngOnInit(): void {
+		this.route.params.forEach((params: Params) => {
+		  if (params['id'] !== undefined) {
+			const id: number = params['id'];
+			this.dogRecordService.viewDogRecord(id).subscribe({
+				next: (dog: Dog) => {
+					this.dog = dog;
+				},
+				error: (err: any) => {
+					console.error(err);
+				}
+			});
+	
+		  } else {
+			// this.navigated = false;
+			// this.hero = new Hero();
+		  }
+		});	}
+}
