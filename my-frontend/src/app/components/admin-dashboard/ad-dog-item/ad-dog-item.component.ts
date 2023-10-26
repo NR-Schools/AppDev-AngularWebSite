956014648e4c34b37621b196f6ef5814b4492717
Tuna <98098486+Null-Root@users.noreply.github.com>
2023-10-26 15:51:18 +0800

    Admin View [Images] Added

--- my-frontend/src/app/components/admin-dashboard/ad-dog-item/ad-dog-item.component.ts
+++ my-frontend/src/app/components/admin-dashboard/ad-dog-item/ad-dog-item.component.ts
@@ -1,17 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Dog } from 'src/app/models/dog';
 import { DogRecordService } from 'src/app/services/dog-record.service';
+import { ImageUtils } from 'src/app/utils/image-utils';
 
 @Component({
 	selector: 'ad-dog-item',
 	templateUrl: './ad-dog-item.component.html',
 	styleUrls: ['./ad-dog-item.component.css']
 })
-export class AdDogItemComponent {
+export class AdDogItemComponent implements OnInit {
 	@Input({ required: true }) dogItem!: Dog;
 	thumbnail: any;
 
 	constructor(private dogRecordService: DogRecordService) {}
+
+	ngOnInit(): void {
+		// Show Image
+		this.thumbnail = ImageUtils.base64ToImage(this.dogItem.photoBytes);
+	}
 
 	onItemRemove(): void {
 		console.log(this.dogItem.id!);
