5b70107d427f0a94a2162b7ba2c871de25a71eef
Tuna <98098486+Null-Root@users.noreply.github.com>
2023-10-26 23:01:37 +0800

    Add Filters to Admin Dashboard

--- my-frontend/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ my-frontend/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -10,7 +10,16 @@ import { Router } from '@angular/router';
 	styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
-	dogs: Dog[] = [];
+	dogs: Array<Dog> = [];
+	filteredDogs: Array<Dog> = [];
+
+	// Filter
+	nameFilter: string = '';
+	breedFilter: string = '';
+	colorFilter: string = '';
+	sizeFilter: string = '';
+	sexFilter: string = '';
+	ageFilter: string = '';
 
 	constructor(private router: Router,
 		private accountService: AccountService,
@@ -31,5 +40,47 @@ export class AdminDashboardComponent implements OnInit {
 				console.log(err);
 			}
 		});
+	}
+
+	// Filters
+	onFilterApplied() {
+		const nameQuery = this.nameFilter.toLowerCase();
+		const breedQuery = this.breedFilter.toLowerCase();
+		const colorQuery = this.colorFilter.toLowerCase();
+		const sizeQuery = this.sizeFilter.toLowerCase();
+		const sexQuery = this.sexFilter.toLowerCase();
+		const ageQuery = this.ageFilter;
+
+		this.filteredDogs = this.dogs;
+
+		if (nameQuery.trim() !== "")
+			this.filteredDogs = this.filteredDogs.filter(dog => dog.name.toLowerCase().includes(nameQuery))
+
+		if (breedQuery.trim() !== "")
+			this.filteredDogs = this.filteredDogs.filter(dog => dog.breed.toLowerCase().includes(breedQuery))
+
+		if (colorQuery.trim() !== "")
+			this.filteredDogs = this.filteredDogs.filter(dog => dog.colorCoat.toLowerCase().includes(colorQuery))
+
+		if (sizeQuery.trim() !== "")
+			this.filteredDogs = this.filteredDogs.filter(dog => dog.size.toLowerCase().includes(sizeQuery))
+
+		if (sexQuery.trim() !== "")
+			this.filteredDogs = this.filteredDogs.filter(dog => dog.sex.toLowerCase().includes(sexQuery))
+
+		console.log(ageQuery)
+		if (ageQuery !== "" && ageQuery !== null)
+			this.filteredDogs = this.filteredDogs.filter(dog => dog.age.toString().toLowerCase().includes(ageQuery));
+	}
+
+	onFiltersReset() {
+		this.breedFilter = "";
+		this.nameFilter = "";
+		this.colorFilter = "";
+		this.sizeFilter = "";
+		this.sexFilter = "";
+		this.ageFilter = "";
+
+		this.filteredDogs = this.dogs;
 	}
 }
