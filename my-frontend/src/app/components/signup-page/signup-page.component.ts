c0bd8fc64a26d0ac7a3845b30a9130a748ecbd44
eydriyan <115596606+eydriyan@users.noreply.github.com>
2023-10-26 19:14:20 +0800

    frontend and routes update

--- my-frontend/src/app/components/signup-page/signup-page.component.ts
+++ my-frontend/src/app/components/signup-page/signup-page.component.ts
@@ -1,45 +1,51 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AccountService } from '../../services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
-	selector: 'app-signup-page',
-	templateUrl: './signup-page.component.html',
-	styleUrls: ['./signup-page.component.css']
+  selector: 'app-signup-page',
+  templateUrl: './signup-page.component.html',
+  styleUrls: ['./signup-page.component.css'],
 })
 export class SignupPageComponent {
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private accountService: AccountService
+  ) {}
 
-	constructor(private formBuilder: FormBuilder,
-				private accountService: AccountService) { }
+  signupFormGroup = this.formBuilder.group({
+    Email: ['', Validators.required],
+    Password: ['', Validators.required],
+    ConfirmPassword: ['', Validators.required],
+  });
 
-	signupFormGroup = this.formBuilder.group({
-		Email: ['', Validators.required],
-		Password: ['', Validators.required],
-		ConfirmPassword: ['', Validators.required],
-	});
+  signupAccount() {
+    // Get Values From Controls
+    const Email = this.signupFormGroup.get('Email')?.value as string;
+    const Password = this.signupFormGroup.get('Password')?.value as string;
+    const ConfirmPassword = this.signupFormGroup.get('ConfirmPassword')
+      ?.value as string;
 
-	signupAccount() {
-		// Get Values From Controls
-		const Email = this.signupFormGroup.get("Email")?.value as string;
-		const Password = this.signupFormGroup.get("Password")?.value as string;
-		const ConfirmPassword = this.signupFormGroup.get("ConfirmPassword")?.value as string;
+    // Check Password == ConfirmPassword
+    if (Password !== ConfirmPassword) {
+      window.alert('"Password" does not match "ConfirmPassword"');
+      return;
+    }
 
-		// Check Password == ConfirmPassword
-		if (Password !== ConfirmPassword) {
-			window.alert("\"Password\" does not match \"ConfirmPassword\"")
-			return;
-		}
-
-		// Signup
-		this.accountService.signUp(Email, Password).subscribe({
-			next: (value: boolean) => {
-				if (value) window.alert("Account Signed Up Successfully!");
-				else window.alert("Account Sign Up Failed!");
-			},
-			error: (err: any) => {
-				window.alert("Error encountered!");
-				console.log(err);
-			}
-		});
-	}
+    // Signup
+    this.accountService.signUp(Email, Password).subscribe({
+      next: (value: boolean) => {
+        if (value) {
+          window.alert('Account Signed Up Successfully!');
+          this.router.navigate(['/login']);
+        } else window.alert('Account Sign Up Failed!');
+      },
+      error: (err: any) => {
+        window.alert('Error encountered!');
+        console.log(err);
+      },
+    });
+  }
 }
--- my-frontend/src/app/components/user-dog-requests/udr-dog-adopt-item/udr-dog-adopt-item.component.ts
+++ my-frontend/src/app/components/user-dog-requests/udr-dog-adopt-item/udr-dog-adopt-item.component.ts
@@ -2,31 +2,36 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Dog } from 'src/app/models/dog';
 import { DogRecordService } from 'src/app/services/dog-record.service';
 import { ImageUtils } from 'src/app/utils/image-utils';
+import { Router } from '@angular/router';
 @Component({
-	selector: 'udr-dog-adopt-item',
-	templateUrl: './udr-dog-adopt-item.component.html',
-	styleUrls: ['./udr-dog-adopt-item.component.css'],
+  selector: 'udr-dog-adopt-item',
+  templateUrl: './udr-dog-adopt-item.component.html',
+  styleUrls: ['./udr-dog-adopt-item.component.css'],
 })
 export class UdrDogAdoptItemComponent implements OnInit {
-	@Input({ required: true }) dogItem!: Dog;
-	thumbnail: any;
+  @Input({ required: true }) dogItem!: Dog;
+  thumbnail: any;
 
-	constructor(private dogRecordService: DogRecordService) { }
+  constructor(
+    private router: Router,
+    private dogRecordService: DogRecordService
+  ) {}
 
-	ngOnInit(): void {
-		this.thumbnail = ImageUtils.base64ToImage(this.dogItem.photoBytes);
-	}
+  ngOnInit(): void {
+    this.thumbnail = ImageUtils.base64ToImage(this.dogItem.photoBytes);
+  }
 
-	onUserCancelDogAdoptRequest() {
-		console.log(this.dogItem);
+  onUserCancelDogAdoptRequest() {
+    console.log(this.dogItem);
 
-		this.dogRecordService.userCancelDogAdopt(this.dogItem).subscribe({
-			next: (value: boolean) => {
-				console.log(value);
-			},
-			error: (err: any) => {
-				console.log(err);
-			},
-		});
-	}
+    this.dogRecordService.userCancelDogAdopt(this.dogItem).subscribe({
+      next: (value: boolean) => {
+        this.router.navigate(['/user-dog-requests']);
+        console.log(value);
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
+  }
 }
